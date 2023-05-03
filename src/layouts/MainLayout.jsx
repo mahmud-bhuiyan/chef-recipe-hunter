5192075814f70b8e7fb893317598848fd7fd7b87
mahmud-bhuiyan <bh.mahmud@gmail.com>
2023-05-04 02:32:03 +0600

    navbar and banner added

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../pages/Shared/Header/Header";
 import Footer from "../pages/Shared/Footer/Footer";
+import { Container } from "react-bootstrap";
 
 const MainLayout = () => {
   return (
     <div>
       <Header></Header>
-      <Outlet></Outlet>
+      <Container>
+        <Outlet></Outlet>
+      </Container>
       <Footer></Footer>
     </div>
   );
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,11 +1,40 @@
 import React from "react";
+import { Nav, Navbar, Button, Image } from "react-bootstrap";
+import { FaUserCircle } from "react-icons/fa";
+import "./Header.css";
 
-const Header = () => {
+function Header() {
   return (
-    <div>
-      <h3>Header</h3>
-    </div>
+    <>
+      <Navbar bg="light" expand="md" fixed="top">
+        <div className="container">
+          <Navbar.Brand className="fw-bold" to="/">
+            Food Cravings
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbar-nav" />
+          <Navbar.Collapse id="navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link to="/">Home</Nav.Link>
+              <Nav.Link to="/blog">Blog</Nav.Link>
+            </Nav>
+            <FaUserCircle
+              className="me-2"
+              style={{ fontSize: "2rem" }}
+            ></FaUserCircle>
+            <Button variant="primary">Login</Button>
+          </Navbar.Collapse>
+        </div>
+      </Navbar>
+      <div className="banner">
+        <h3 className="text-dark">
+          Learn the <span className="highlight">Art</span> of Cooking Here
+        </h3>
+        <p className="text-dark">
+          Discover the joy of cooking with our recipes and tutorials
+        </p>
+      </div>
+    </>
   );
-};
+}
 
 export default Header;
