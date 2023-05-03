d63ac8d800e6e2b83abdda044d19fb1d10ffd30c
mahmud-bhuiyan <bh.mahmud@gmail.com>
2023-05-04 01:22:04 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDKpq5oaPVBKyqQWmcs19lqqkETU9Pu48k",
+  authDomain: "food-cravings-4b6e0.firebaseapp.com",
+  projectId: "food-cravings-4b6e0",
+  storageBucket: "food-cravings-4b6e0.appspot.com",
+  messagingSenderId: "246882343266",
+  appId: "1:246882343266:web:2cc8e695d4582b6ef1743b",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
