5cf3f3670425bd053c36f7210d59ffdb3f3952e9
mahmud-bhuiyan <bh.mahmud@gmail.com>
2023-05-04 19:44:44 +0600

    error page added

---
+++ README.md
@@ -0,0 +1,13 @@
+## Website Name: Food Cravings
+
+## Live Link:
+
+## Features
+
+The application consists of the following components:
+
+- `Blog`: Displays all the question ans.
+
+- `ErrorPage`: Displays an error page.
+
+The application uses `react-router-dom` for routing and navigation between different pages.
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -5,11 +5,13 @@ import Home from "../pages/Home/Home";
 import Blog from "../pages/Blog/Blog";
 import Login from "../pages/Login/Login";
 import Register from "../pages/Register/Register";
+import ErrorPage from "../pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout></MainLayout>,
+    errorElement: <ErrorPage></ErrorPage>,
     children: [
       {
         path: "/",
